45e901a865de3b9b082605850398ec77fb097e5e
Pedro Perrone <pedro.perrone@gmail.com>
2017-05-07 18:57:05 +0000

    updated social buttons style

--- js/init.js
+++ js/init.js
@@ -2,6 +2,12 @@
   $(function(){
 
     $('.button-collapse').sideNav();
+    
+    $(document).ready(function(){
+      $('.tooltipped').tooltip({delay: 50});
+    });
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
+     
+
